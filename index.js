da349e3c731f191d44939dc7070ef8c2158eeebc
S  1 <lokohopo@gmail.com>
2020-11-12 17:50:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,5 +19,16 @@ function add(a,b) {
 }
 
 function subtract (a,b) {
-  return 
+  return "a-b";
+}
+
+function multiply(a,b) {
+  return "a*b";
+}
+
+function divide(a,b) {
+  return "a/b";
+}
+
+
 }
