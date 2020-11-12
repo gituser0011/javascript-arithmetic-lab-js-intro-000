0599e153f0ce5d69e077d0920a6e0841f68c1c58
S  1 <lokohopo@gmail.com>
2020-11-12 17:48:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 function add() {
   return "+";
 }
+
+function subtract() {
+  return "-";
+}
+
+function multiply() {
+  return "*";
+}
+
+function divide() {
+  return "/";
+}
+
+function add(a,b) {
+  return "a + b";
+}
+
+function subtract (a,b) {
+  return 
+}
